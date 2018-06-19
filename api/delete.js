2228e1aa6786d9612fd40cf365f3b05c3e6ee1dd
Justine Jay Fernandez <justinefernandez21@gmail.com>
2018-06-19 19:05:01 +0800

    Modified: Delete.js

--- api/delete.js
+++ api/delete.js
@@ -6,51 +6,72 @@ var tag = require('./models/Tags.js');
 
 module.exports.deleteUser = (event, context, callback) => {
     context.callbackWaitsForEmptyEventLoop = false;
+    var data = JSON.parse(event.body);
     user.destroy({
-        id: event
-    })
+        where: {
+            id: data.id
+        }
+    }).then(function(rowsDeleted){
+        if (rowsDeleted > 0) {
+            const response = {
+                statusCode: 200,
+                body: JSON.stringify({}),
+            };
+       
+            callback(null, response);
+        } else {
+            callback(null, {
+                statusCode: 401,
+                headers: { 'Content-Type': 'text/plain' },
+                body: 'User not found',
+            });
+        }
+    
+    }, function(err){
+        console.error(err);
+        callback(null, {
+            statusCode: error.statusCode || 501,
+            headers: { 'Content-Type': 'text/plain' },
+            body: 'Could not remove user',
+        });
+        return;
+    });
 };
 
 module.exports.deletePost = (event, context, callback) => {
     context.callbackWaitsForEmptyEventLoop = false;
-    const timestamp = new Date().getTime();
     var data = JSON.parse(event.body);
-
-    const params = {
-        username: data.username,
-        password: data.password,
-        createdAt: timestamp,
-        updatedAt: timestamp,
-    };
-
-    post.create(params).then( 
-        post => {
+    user.destroy({
+        where: {
+            id: data.id
+        }
+    }).then(function(rowsDeleted){
+        if (rowsDeleted > 0) {
             const response = {
                 statusCode: 200,
-                body: JSON.stringify(params),
+                body: JSON.stringify({}),
             };
+       
             callback(null, response);
+        } else {
+            callback(null, {
+                statusCode: 401,
+                headers: { 'Content-Type': 'text/plain' },
+                body: 'User not found',
+            });
+        }
+    
+    }, function(err){
+        console.error(err);
+        callback(null, {
+            statusCode: error.statusCode || 501,
+            headers: { 'Content-Type': 'text/plain' },
+            body: 'Could not remove post',
         });
+        return;
+    });
 };
 
-module.exports.deleteUser = (event, context, callback) => {
-    context.callbackWaitsForEmptyEventLoop = false;
-    const timestamp = new Date().getTime();
-    var data = JSON.parse(event.body);
+module.exports.deleteTag = (event, context, callback) => {
 
-    const params = {
-        username: data.username,
-        password: data.password,
-        createdAt: timestamp,
-        updatedAt: timestamp,
-    };
-
-    tag.create(params).then( 
-        tag => {
-            const response = {
-                statusCode: 200,
-                body: JSON.stringify(params),
-            };
-            callback(null, response);
-        });
 };
