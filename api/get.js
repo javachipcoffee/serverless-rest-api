fe5f75fabb249f140e4a2629253a1cc38ccb0784
Justine <justinefernandez212gmail.com>
2018-06-20 06:03:12 +0800

    Added: update on post and tags, and added a get function on post, users, and tags

--- api/get.js
+++ api/get.js
@@ -3,3 +3,61 @@
 var user = require('./models/Users.js');
 var post = require('./models/Posts.js');
 var tag = require('./models/Tags.js');
+
+
+module.exports.getUser = (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false; 
+    user.findById(event.pathParameters.id).then(user => {
+        if(user === null){
+            callback(null, {
+                statusCode: 501,
+                headers: { 'Content-Type': 'text/plain' },
+                body: 'Could not get User.',
+            });
+            return;
+        }
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(user),
+        };
+        callback(null, response);
+    });  
+};
+
+module.exports.getPost = (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false; 
+    post.findById(event.pathParameters.id).then(user => {
+        if(user === null){
+            callback(null, {
+                statusCode: 501,
+                headers: { 'Content-Type': 'text/plain' },
+                body: 'Could not get post data.',
+            });
+            return;
+        }
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(user),
+        };
+        callback(null, response);
+    });  
+};
+
+module.exports.getTag = (event, context, callback) => {
+    context.callbackWaitsForEmptyEventLoop = false; 
+    tag.findById(event.pathParameters.id).then(user => {
+        if(user === null){
+            callback(null, {
+                statusCode: 501,
+                headers: { 'Content-Type': 'text/plain' },
+                body: 'Could not get tag.',
+            });
+            return;
+        }
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(user),
+        };
+        callback(null, response);
+    });  
+};
--- api/update.js
+++ api/update.js
@@ -54,3 +54,105 @@ module.exports.updateUser = (event, context, callback) => {
         });
     });
 };
+
+module.exports.updatePost = (event, context, callback) => {
+    context.callbackWaitsFupdateV2orEmptyEventLoop = false; 
+    const timestamp = new Date().getTime();
+    var data = event.body;
+    if(typeof event.body !== 'object'){
+        data = JSON.parse(event.body);
+    }
+    if (typeof data.username !== 'string' || typeof data.password !== 'string') {
+        console.error('Validation Failed');
+        callback(null, {
+            statusCode: 400,
+            headers: { 'Content-Type': 'text/plain' },
+            body: 'Couldn\'t update the User data',
+        });
+        return;
+    }
+    const params = {
+        username: data.username,
+        post: data.post,
+        tag: data.tag,
+        updatedAt: timestamp,
+    };
+    post.update(
+        params,
+        { 'where': { 'id' : event.pathParameters.id } }
+    ).then( result => {
+        if(result){ 
+            post.findById(event.pathParameters.id).then( posts => {
+                const response = {
+                    statusCode: 200,
+                    body: JSON.stringify(posts),
+                };
+                callback(null, response);
+            });
+            return; 
+        }
+        callback(null, {
+            statusCode: 501,
+            headers: { 'Content-Type': 'text/plain' },
+            body: 'Could not fetch Post.',
+        });
+  
+    }, function(err){
+        console.error(err);
+        callback(null, {
+            statusCode: err.statusCode || 501,
+            headers: { 'Content-Type': 'text/plain' },
+            body: 'Could not update Post. Error: '+err,
+        });
+    });
+};
+
+module.exports.updateTag = (event, context, callback) => {
+    context.callbackWaitsFupdateV2orEmptyEventLoop = false; 
+    const timestamp = new Date().getTime();
+    var data = event.body;
+    if(typeof event.body !== 'object'){
+        data = JSON.parse(event.body);
+    }
+    if (typeof data.username !== 'string' || typeof data.password !== 'string') {
+        console.error('Validation Failed');
+        callback(null, {
+            statusCode: 400,
+            headers: { 'Content-Type': 'text/plain' },
+            body: 'Couldn\'t update the User data',
+        });
+        return;
+    }
+    const params = {
+        tag: data.tag,
+        updatedAt: timestamp,
+    };
+    tag.update(
+        params,
+        { 'where': { 'id' : event.pathParameters.id } }
+    ).then( result => {
+        if(result){ 
+            tag.findById(event.pathParameters.id).then( tags => {
+                const response = {
+                    statusCode: 200,
+                    body: JSON.stringify(tags),
+                };
+                callback(null, response);
+            });
+            return; 
+        }
+        callback(null, {
+            statusCode: 501,
+            headers: { 'Content-Type': 'text/plain' },
+            body: 'Could not fetch Post.',
+        });
+  
+    }, function(err){
+        console.error(err);
+        callback(null, {
+            statusCode: err.statusCode || 501,
+            headers: { 'Content-Type': 'text/plain' },
+            body: 'Could not update Post. Error: '+err,
+        });
+    });
+};
